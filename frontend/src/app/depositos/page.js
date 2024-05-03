ec586eb131f2b5bc6023832549144c13acc14638
gissgroa <57072138+gdroa-edu@users.noreply.github.com>
2024-05-03 15:34:37 -0400

    estructura del frontend

--- frontend/src/app/depositos/page.js
+++ frontend/src/app/depositos/page.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Sidebar from '@/components/sidebar';
 
-export const Depositos = () => {
+const Depositos = () => {
   return (
-    <div>Depósitos</div>
+    <div className="flex h-screen w-full bg-ui-background p-2 text-ui-text">
+        <Sidebar/>
+          <div className="flex flex-col w-full h-full p-5 rounded-lg bg-ui-cardbg">
+          <h1 className='mb-4 text-l font-semibold normal-case tracking-tight'>Depósitos</h1> 
+      </div>
+        </div>
+    
   )
 }
 
