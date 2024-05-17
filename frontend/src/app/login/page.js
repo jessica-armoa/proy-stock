76bdaa775f5c4c63f0d5ec5aa94049949c8be663
FatimaVenialgo <favenialgo717@gmail.com>
2024-05-17 11:38:21 -0400

    Agregue validacion en los campos del login

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -13,20 +13,26 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    // Verificar que los campos no estén vacíos
+    if (!username || !password) {
+      setError('Por favor, complete todos los campos.');
+      return;
+    }
+
     try {
-      console.log("Hola");
       const response = await AuthController.login({ "username": username, "password": password });
       console.log(response.data);
       localStorage.setItem("Token", response.data.token);
       localStorage.setItem("isAuthenticated", true);
       navigate('/productos');
-    } catch (err) {console.error("Login error:", err);
+    } catch (err) {
       if (err.response) {
         setError(err.response.data);
       } else if (err.request) {
-        setError('No response received from server');
+        setError('No hay respuesta del Servidor');
       } else {
-        setError('Error setting up request');
+        setError('Error en la petición');
       }
     }
   };
@@ -34,10 +40,10 @@ const Login = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
-        <h1 className="text-2xl font-bold mb-6">Login</h1>
+        <h1 className="text-2xl font-bold mb-6">Inicio De Sesión</h1>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label htmlFor="username" className="block text-gray-700 mb-2">Username:</label>
+            <label htmlFor="username" className="block text-gray-700 mb-2">Usuario:</label>
             <input
               type="text"
               id="username"
@@ -47,7 +53,7 @@ const Login = () => {
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="password" className="block text-gray-700 mb-2">Password:</label>
+            <label htmlFor="password" className="block text-gray-700 mb-2">Contraseña:</label>
             <input
               type="password"
               id="password"
@@ -65,7 +71,7 @@ const Login = () => {
             type="submit"
             className="w-full bg-indigo-500 text-white p-3 rounded-lg font-semibold hover:bg-indigo-600"
           >
-            Login
+            Iniciar Sesión
           </button>
         </form>
       </div>
