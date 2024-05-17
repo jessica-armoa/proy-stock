cb0f1306702bc3e0343c1ea2e9a14d6aed016680
jangel-miranda <juan.miranda@fiuni.edu.py>
2024-05-17 13:20:41 -0400

    'actualizacion_formulario_produdcto'

--- frontend/src/app/productos/CrearProducto.jsx
+++ frontend/src/app/productos/CrearProducto.jsx
@@ -1,16 +1,16 @@
 import Sidebar from "@/components/sidebar";
-import FormularioProductos from "../../components/FormularioProductos";
+import FormularioProductos from "./FormularioProductos";
 
 
-export default function CrearProducto(){
-    return(
+export default function CrearProducto() {
+    return (
         <div className="flex h-screen w-full bg-ui-background p-2 text-ui-text">
-        <Sidebar/>
-          <div className="flex flex-col w-full h-full p-5 rounded-lg bg-ui-cardbg">
-          <h1 className='mb-4 text-l font-semibold normal-case tracking-tight'>Crear producto</h1> 
-          <FormularioProductos/>
-      </div>
+            <Sidebar />
+            <div className="flex flex-col w-full h-full p-5 rounded-lg bg-ui-cardbg">
+                <h1 className='mb-4 text-l font-semibold normal-case tracking-tight'>Crear producto</h1>
+                <FormularioProductos />
+            </div>
         </div>
-        
+
     )
 }
