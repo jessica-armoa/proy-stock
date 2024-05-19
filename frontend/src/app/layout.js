d36998b5fc18edd39742d8908c99935bf3178ece
jangel-miranda <juan.miranda@fiuni.edu.py>
2024-05-18 22:16:46 -0400

    formulario ya guarda

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -13,6 +13,13 @@ import Dashboard from './page';
 import Movimientos from './movimientos/page';
 import Depositos from './depositos/page';
 import CrearProducto from './productos/CrearProducto';
+import Marcas from './marcas/page';
+import CrearMarca from './marcas/CrearMarca';
+import CrearDeposito from './depositos/CrearDeposito';
+import Proveedores from './proveedores/page';
+import CrearProveedor from './proveedores/CrearProveedor';
+import CrearFerreteria from './ferreterias/CrearFerreteria';
+import Ferreterias from './ferreterias/page';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,19 +28,34 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Router>
         <Routes>
+          <Route path="/depositos" element={< Depositos />} />
+          <Route path="/depositos/nuevo" element={< CrearDeposito />} />
+
+          <Route path="/ferreterias" element={< Ferreterias />} />
+          <Route path="/ferreterias/nuevo" element={< CrearFerreteria />} />
+
+
+          <Route path="/marcas" element={< Marcas />} />
+          <Route path="/marcas/nuevo" element={< CrearMarca />} />
+
+          <Route path="/movimientos" element={< Movimientos />} />
+
           <Route path="/productos/detalle/:id" element={<Detalle />} />
           <Route path="/productos" element={<Productos />} />
-          <Route path="/" element={< Dashboard />} />
-          <Route path="/movimientos" element={< Movimientos />} />
-          <Route path="/depositos" element={< Depositos />} />
           <Route path="/productos/nuevo" element={< CrearProducto />} />
+
+          <Route path="/proveedores" element={< Proveedores />} />
+          <Route path="/proveedores/nuevo" element={< CrearProveedor />} />
+
+          <Route path="/" element={< Dashboard />} />
+
         </Routes>
-        </Router>
+      </Router>
       <body className={inter.className}>
 
 
       </body>
 
-          </html>
+    </html>
   );
 }
