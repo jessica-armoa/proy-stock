d7dfd00fcf340c7f5dead33f1854cd77b3b0f5b9
gissgroa <57072138+gdroa-edu@users.noreply.github.com>
2024-06-05 13:22:33 -0400

    corregida ruta api

--- frontend/src/libs/ProductsController.jsx
+++ frontend/src/libs/ProductsController.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = process.env.NEXT_PUBLIC_API_URL+"api/producto";
+const api = process.env.NEXT_PUBLIC_API_URL+"producto";
 
 class ProductsController {
   getProducts() {
