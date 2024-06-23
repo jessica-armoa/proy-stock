c32c1441b69f1b84bed3310c05c7cc07e0015853
gissgroa <57072138+gdroa-edu@users.noreply.github.com>
2024-06-22 21:59:55 -0400

    fixes a todos los formularios

--- frontend/src/app/depositos/page.js
+++ frontend/src/app/depositos/page.js
@@ -169,7 +169,7 @@ const Depositos = () => {
   return (
     <div className="flex h-screen w-full bg-ui-background p-2 text-ui-text">
       <Sidebar />
-      <div className="flex flex-col w-full h-full p-5 rounded-lg bg-ui-cardbg">
+      <div className="flex flex-col w-full h-full p-5 rounded-lg bg-ui-cardbg overflow-y">
         <h1 className='mb-4 text-l font-semibold normal-case tracking-tight'>Depósitos</h1> 
         <div className="flex items-center justify-end space-x-2">
           <Button
