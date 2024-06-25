08960053864fff3f2e7871adc5621bc541629bf7
sebasbogado <101952377+sebasbogado@users.noreply.github.com>
2024-06-25 11:20:23 -0400

    Correccion de errores, la funcion de actualizar costo PPP se elimina, correcion en controller de motivo por tipo de movimiento faltaba "api/"

--- backend/api/Controllers/MotivoPorTipoMovimientoController.cs
+++ backend/api/Controllers/MotivoPorTipoMovimientoController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
 {
-    [Route("motivo-por-tipo-de-movimiento")]
+    [Route("api/motivo-por-tipo-de-movimiento")]
     [ApiController]
     public class MotivoPorTipoMovimientoController : ControllerBase
     {
--- backend/api/Dtos/DetalleDeMovimiento/DetalleDeMovimientoDto.cs
+++ backend/api/Dtos/DetalleDeMovimiento/DetalleDeMovimientoDto.cs
@@ -12,7 +12,7 @@ namespace api.Dtos.DetalleDeMovimiento
         public int Int_cantidad { get; set; }
         public decimal Dec_costo { get; set; }
         public int? MovimientoId { get; set; }
-        public string Str_movimiento { get; set; } = String.Empty;
+        //public string Str_movimiento { get; set; } = String.Empty;
         public int? ProductoId { get; set; }
         public string Str_producto { get; set; } = String.Empty;
         public bool Bool_borrado { get; set; } = false;
