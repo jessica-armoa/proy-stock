fab0dece7b309bec88a6fe559a5eb1a4274d6627
gissgroa <57072138+gdroa-edu@users.noreply.github.com>
2024-06-28 03:08:45 -0400

    fixed timbrado

--- frontend/src/app/movimientos/nuevo/FormularioMovimientos.jsx
+++ frontend/src/app/movimientos/nuevo/FormularioMovimientos.jsx
@@ -429,7 +429,7 @@ export default function FormularioMovimientos() {
                                         <input
                                             type="text"
                                             id="timbradoRemision"
-                                            value={timbradoRemision}
+                                            value={timbradoRemision.str_timbrado}
                                             onChange={(e) => setTimbradoRemision(e.target.value)}
                                             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm sm:text-sm"
                                             required={isTransferencia}
